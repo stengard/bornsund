6ad6683215aa007b4574317f6fd7ef648d454bd0
Martin <mstengard@gmail.com>
2018-12-09 13:03:24 +0100

    gallery page

---
+++ src/cms/preview-templates/GalleryPagePreview.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { GalleryPageTemplate } from "../../templates/gallery-page";
+
+const GalleryPagePreview = ({ entry, widgetFor }) => (
+  <GalleryPageTemplate
+    title={entry.getIn(["data", "title"])}
+    content={widgetFor("body")}
+  />
+);
+
+GalleryPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func
+  }),
+  widgetFor: PropTypes.func
+};
+
+export default GalleryPagePreview;
--- src/templates/gallery-page.js
+++ src/templates/gallery-page.js
@@ -4,7 +4,7 @@ import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+export const GalleryPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content;
 
   return (
@@ -25,18 +25,18 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   );
 };
 
-AboutPageTemplate.propTypes = {
+GalleryPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
   contentComponent: PropTypes.func
 };
 
-const AboutPage = ({ data }) => {
+const GalleryPage = ({ data }) => {
   const { markdownRemark: post } = data;
 
   return (
     <Layout>
-      <AboutPageTemplate
+      <GalleryPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
@@ -45,14 +45,14 @@ const AboutPage = ({ data }) => {
   );
 };
 
-AboutPage.propTypes = {
+GalleryPage.propTypes = {
   data: PropTypes.object.isRequired
 };
 
-export default AboutPage;
+export default GalleryPage;
 
-export const aboutPageQuery = graphql`
-  query AboutPage($id: String!) {
+export const galleryPageQuery = graphql`
+  query GalleryPage($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
