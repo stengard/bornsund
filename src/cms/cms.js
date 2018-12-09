8fb4048c780c6ddeba2d155405d86579009c78fb
Martin <mstengard@gmail.com>
2018-12-09 17:46:13 +0100

    changed wrongly spelled import

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,9 +3,9 @@ import CMS from "netlify-cms";
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import ProductPagePreview from "./preview-templates/ProductPagePreview";
-import GammeryPagePreview from "./preview-templates/GammeryPagePreview";
+import GalleryPagePreview from "./preview-templates/GalleryPagePreview";
 
 CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("products", ProductPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
-CMS.registerPreviewTemplate("gallery", GammeryPagePreview);
+CMS.registerPreviewTemplate("gallery", GalleryPagePreview);
